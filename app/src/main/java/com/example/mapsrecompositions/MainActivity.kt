632e67e1fad5560dd45ae27b3d1f611b3922eb06
Lucas Kivi <lucas@livefront.com>
2024-07-15 22:07:32 -0500

    Clean up `MainActivity`

--- app/src/main/java/com/example/mapsrecompositions/MainActivity.kt
+++ app/src/main/java/com/example/mapsrecompositions/MainActivity.kt
@@ -6,38 +6,17 @@ import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.animation.core.tween
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.focusable
-import androidx.compose.foundation.gestures.Orientation
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.offset
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.BottomSheetScaffold
-import androidx.compose.material.Button
-import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.ExperimentalMaterialApi
-import androidx.compose.material.Text
-import androidx.compose.material.rememberSwipeableState
-import androidx.compose.material.swipeable
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Alignment.Companion
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -45,13 +24,10 @@ import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.semantics.clearAndSetSemantics
 import androidx.compose.ui.semantics.contentDescription
 import androidx.compose.ui.semantics.invisibleToUser
-import androidx.compose.ui.semantics.isTraversalGroup
 import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.semantics.traversalIndex
-import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.LayoutDirection
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.example.mapsrecompositions.ui.theme.MapsRecompositionsTheme
 import com.google.android.gms.maps.model.LatLng
 import com.google.maps.android.compose.GoogleMap
@@ -59,9 +35,6 @@ import com.google.maps.android.compose.MapEffect
 import com.google.maps.android.compose.MapsComposeExperimentalApi
 import com.google.maps.android.compose.Marker
 import com.google.maps.android.compose.MarkerState
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
-import kotlin.math.roundToInt
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -218,74 +191,6 @@ fun CustomComponentExample() {
         CustomComposable(
             contentPaddingState = contentPaddingState,
             modifier = Modifier.fillMaxSize(),
-        )
-    }
-}
-
-enum class SwipeableAnchor { Left, Right }
-
-@Composable
-fun SwipeableExample() {
-    val widthDp = 200.dp
-    val heightDp = 100.dp
-    val swipeableSizeDp = heightDp
-    val swipeableSizePx = swipeableSizeDp.toPx()
-
-    val swipeableState = rememberSwipeableState(initialValue = SwipeableAnchor.Left)
-    val scope = rememberCoroutineScope()
-
-
-    Box(
-        modifier = Modifier
-            .background(color = Color.LightGray)
-            .size(width = widthDp, height = heightDp)
-    ) {
-        val anchors = remember {
-            mapOf(
-                0f to SwipeableAnchor.Left,
-                swipeableSizePx to SwipeableAnchor.Right,
-            )
-        }
-
-        Spacer(
-            modifier = Modifier
-                .align(Alignment.CenterStart)
-                .size(swipeableSizeDp)
-                .offset {
-                    IntOffset(
-                        x = swipeableState.offset.value.roundToInt(),
-                        y = 0,
-                    )
-                }
-                .swipeable(
-                    anchors = anchors,
-                    state = swipeableState,
-                    orientation = Orientation.Horizontal,
-                )
-                .background(color = Color.Blue)
-                .clickable {
-                    scope.launch {
-                        scope
-                            .launch {
-                                Log.d("DAVID", "animate start")
-                                swipeableState.animateTo(SwipeableAnchor.Right, tween(4000))
-                                Log.d("DAVID", "animate end")
-                            }
-                            .invokeOnCompletion {
-                                Log.d("DAVID", "animate completed ${it?.message}")
-                            }
-                        delay(100)
-                        scope
-                            .launch {
-                                Log.d("DAVID", "snap start ${swipeableState.isAnimationRunning}")
-                                swipeableState.snapTo(SwipeableAnchor.Left)
-                                Log.d("DAVID", "snap end")
-                            }
-                            .invokeOnCompletion {
-                                Log.d("DAVID", "snap completed ${it?.message}")
-                            }
-                    }
-                }
         )
     }
 }
